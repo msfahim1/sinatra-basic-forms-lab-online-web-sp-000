a3e28c7d3d52f57d4e56f83ba2a1abadabe947b3
Michael Fahim <michael_fahim@yahoo.com>
2020-08-05 23:43:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,18 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get "/" do
+        erb :index
+    end
 
-  get '/' do
-    erb :index
-  end
+    get "/new" do
+        erb :create_puppy
+    end
 
-  get '/new' do
-    erb :create_puppy
-  end
-  post "/puppy" do
-       @breed = params["breed"]
-       @name = params["name"]
-       @age = params["months_old"]
-       erb :display_puppy
-   end
+    post "/puppy" do
+        @breed = params["breed"]
+        @name = params["name"]
+        @age = params["months_old"]
+        erb :display_puppy
+    end
 end
