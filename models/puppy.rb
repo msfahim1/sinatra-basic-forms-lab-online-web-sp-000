5a1164d0659e7f25a0fa69add93beda5855d3f50
Michael Fahim <michael_fahim@yahoo.com>
2020-08-05 23:33:12 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,3 +2,11 @@
 class Puppy
   attr_accessor :name, :breed, :age
   attr_reader :name, :breed, :age
+
+def initialize(name, breed, age)
+  @name = name
+  @breed = breed
+  @age = age
+end
+  
+end
